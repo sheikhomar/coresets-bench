041f1230064463aa85d439266a4966ecda9f6549
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2021-07-19 13:44:25 +0200

    Remove unnecessary libraries.

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,9 +1,9 @@
 sudo apt-get update
 
 # Install libraries
-sudo apt-get install -y build-essential libblas-dev liblapack-dev libssl-dev g++ python-dev autotools-dev libicu-dev libbz2-dev libboost-all-dev checkinstall 
-sudo apt-get install -y python-setuptools python-pip python-smbus
-sudo apt-get install -y libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev wget curl llvm libncurses5-dev xz-utils tk-dev liblzma-dev python-openssl libreadline-dev libsqlite3-dev unixodbc-dev
+sudo apt-get install -y build-essential libblas-dev liblapack-dev libssl-dev g++ python-dev autotools-dev libicu-dev libbz2-dev libboost-all-dev checkinstall libreadline-dev
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev wget curl llvm libncurses5-dev xz-utils tk-dev liblzma-dev python-openssl libreadline-dev unixodbc-dev
 
 cd ~
 mkdir temp
