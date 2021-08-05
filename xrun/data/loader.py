a1669f444d4062087ed1f0b448e3f1da1c7f55f9
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2021-08-05 15:16:01 +0200

    Code for loading benchmark data.

--- xrun/data/loader.py
+++ xrun/data/loader.py
@@ -112,6 +112,28 @@ def load_sift10m_dataset(input_path: str):
     return data
 
 
+def load_benchmark_dataset(input_path: str):
+    dimensions = 0 # The `nonlocal dimensions` below in iter_func() binds to this variable.
+    def iter_func():
+        nonlocal dimensions
+        with gzip.open(input_path,'rt') as f:
+            for line in f:
+                if len(line) > 0:
+                    line = line.rstrip().split(",")
+                    for item in line:
+                        yield float(item)
+            dimensions = len(line)
+
+    print(f"Loading benchmark dataset from {input_path}...")
+    start_time = timer()
+
+    data = np.fromiter(iter_func(), dtype=np.double)
+    data = data.reshape((-1, dimensions))
+    end_time = timer()
+    print(f"Loaded matrix of shape {data.shape} in {end_time - start_time:.2f} secs")
+    return data
+
+
 def load_dataset(input_path: str) -> object:
     loader_fn_map : Dict[str, Callable[[str], object]] = {
         "docword": load_bag_of_words_dataset,
@@ -119,6 +141,7 @@ def load_dataset(input_path: str) -> object:
         "USCensus1990": load_census_dataset,
         "covtype": load_covertype_dataset,
         "sift10m": load_sift10m_dataset,
+        "benchmark": load_benchmark_dataset,
     }
     for name_like, loader_fn in loader_fn_map.items():
         if name_like in input_path:
