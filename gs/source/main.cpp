0736f4e20135dccba643906e65d6196e4372dfc0
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2021-09-10 15:55:46 +0200

    Fix bug.

--- gs/source/main.cpp
+++ gs/source/main.cpp
@@ -79,7 +79,7 @@ void outputResultsToFile(const std::shared_ptr<blaze::DynamicMatrix<double>> ori
   coreset->writeToStream(*originalDataPoints, outData);
 }
 
-int main_new(int argc, char **argv)
+int main(int argc, char **argv)
 {
   if (argc < 8)
   {
@@ -380,50 +380,55 @@ int main_old() {
   // auto parser = CensusParser();
   // auto parsedData = parser.parse("data/raw/USCensus1990.data.txt");
 
-  // auto parser = CovertypeParser();
-  // auto parsedData = parser.parse("data/raw/covtype.data.gz");
+  auto parser = CovertypeParser();
+  auto parsedData = parser.parse("data/raw/covtype.data.gz");
 
-  size_t k = 3; // Number of clusters.
-  size_t T = 20; // Number of target points.
-  double numberOfRaysPerCluster = 10;
-  coresets::RayMaker rayMaker(T, k, numberOfRaysPerCluster);
-  auto coreset = rayMaker.run(data);
+  coresets::StreamKMeans algo(100);
+  auto coreset = algo.run(*parsedData);
+
+  outputResultsToFile(parsedData, coreset, "data");
+
+  // size_t k = 3; // Number of clusters.
+  // size_t T = 20; // Number of target points.
+  // double numberOfRaysPerCluster = 10;
+  // coresets::RayMaker rayMaker(T, k, numberOfRaysPerCluster);
+  // auto coreset = rayMaker.run(data);
   
-  for (size_t i = 0; i < coreset->size(); i++)
-  {
-    auto point = coreset->at(i);
-    printf(point->IsCenter ? "Center" : "Point");
-    printf(" %ld with weight %0.4f\n", point->Index, point->Weight);
-  }
+  // for (size_t i = 0; i < coreset->size(); i++)
+  // {
+  //   auto point = coreset->at(i);
+  //   printf(point->IsCenter ? "Center" : "Point");
+  //   printf(" %ld with weight %0.4f\n", point->Index, point->Weight);
+  // }
 
-  std::cout << "Hello world!\n";
+  // std::cout << "Hello world!\n";
 
-  std::vector<double> x = {-4, -5, -1, 0, 4, -4, -6, 7, 8, 10, 22};
-  std::vector<double> y = {1.0};
-  size_t length = x.size();
-  // size_t k = 10;
-  size_t minK = k;
-  size_t maxK = k;
-  const double * xp(x.data()), * yp (y.data());
-  std::vector<int> clusters(x.size());
-  std::string method = "linear";
-  std::string estimateK = "BIC";
-  std::vector<double> BIC(maxK - minK + 1);
+  // std::vector<double> x = {-4, -5, -1, 0, 4, -4, -6, 7, 8, 10, 22};
+  // std::vector<double> y = {1.0};
+  // size_t length = x.size();
+  // // size_t k = 10;
+  // size_t minK = k;
+  // size_t maxK = k;
+  // const double * xp(x.data()), * yp (y.data());
+  // std::vector<int> clusters(x.size());
+  // std::string method = "linear";
+  // std::string estimateK = "BIC";
+  // std::vector<double> BIC(maxK - minK + 1);
 
-  // cdef double [:] sizes = np.zeros(max_k, dtype=np.float64)
-  std::vector<double> sizes(maxK);
-  std::vector<double> centers(maxK);
-  std::vector<double> withinss(maxK);
+  // // cdef double [:] sizes = np.zeros(max_k, dtype=np.float64)
+  // std::vector<double> sizes(maxK);
+  // std::vector<double> centers(maxK);
+  // std::vector<double> withinss(maxK);
   
-  double * center_p (centers.data()), * sp (sizes.data());
-  double * bp (BIC.data()), * wp (withinss.data());
-  int * cluster_p (clusters.data());
+  // double * center_p (centers.data()), * sp (sizes.data());
+  // double * bp (BIC.data()), * wp (withinss.data());
+  // int * cluster_p (clusters.data());
 
-  kmeans_1d_dp(xp, length, yp, minK, maxK,
-              cluster_p, center_p, wp, sp, bp,
-              estimateK, method, L2);
+  // kmeans_1d_dp(xp, length, yp, minK, maxK,
+  //             cluster_p, center_p, wp, sp, bp,
+  //             estimateK, method, L2);
 
-  std::cout << "Done!\n";
+  // std::cout << "Done!\n";
 
   return 0;
 }
