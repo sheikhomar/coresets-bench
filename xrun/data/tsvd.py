60db0d70f58fbbc01cf28a6534cc03774d0b8dba
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2021-09-25 16:39:18 +0200

    Fix issue with sparse SVD.
    scipy.sparse.linalg.eigen.svds does not return singular values in
    descending order.

--- xrun/data/tsvd.py
+++ xrun/data/tsvd.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from scipy import linalg
 from scipy.sparse import linalg as sparse_linalg, issparse
+from sklearn.utils.extmath import svd_flip, safe_sparse_dot
 
 from xrun.data.loader import load_dataset
 
@@ -19,7 +20,11 @@ def perform_projection(X, target_dim: int):
     # s: The singular values, sorted in descending order.
     # V: Unitary matrix having right singular vectors as rows.
     if issparse(X):
-        U, s, V = sparse_linalg.eigen.svds(A=X, solver='arpack')
+        U, s, V = sparse_linalg.eigen.svds(A=X, k=target_dim, solver='arpack')
+        
+        # svds sorts the singular values in ascending order, reverse it
+        s = s[::-1]
+        U, V = svd_flip(U[:, ::-1], V[::-1])
     else:
         U, s, V = linalg.svd(
             a=X,
