5c4bbae5c8973e94d24a021832339a2e4a874b5f
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2021-09-29 16:05:27 +0200

    Implement sparse matrices.

--- rp/sketch.cpp
+++ rp/sketch.cpp
@@ -13,12 +13,13 @@
 #include <cstdlib>
 #include <stdexcept>
 #include <random>
-
+#include <map>
 #include <algorithm>
 #include <vector>
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <unordered_map>
 
 #include <boost/array.hpp>
 #include <boost/range/algorithm_ext/erase.hpp>
@@ -28,6 +29,9 @@
 #include <boost/iostreams/copy.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
 #include <boost/iostreams/filter/bzip2.hpp>
+#include "robin_hood.h"
+#include "stop_watch.h"
+
 
 #define _XOPEN_SOURCE 700
 
@@ -44,6 +48,283 @@ typedef struct
     uint_fast64_t s0; /* extra bit */
     uint_fast64_t *S; /* array of k full seeds */
 } BCH_conf;
+
+class DokSparseMatrix : boost::noncopyable
+{
+public:
+
+private:
+    size_t _nRows;
+    size_t _nColumns;
+    size_t _rowPower;
+    robin_hood::unordered_node_map<uint64_t, double> data;
+
+public:
+    DokSparseMatrix() { }
+
+    void setSize(size_t nRows, size_t nColumns, size_t nonZeros)
+    {
+        _nRows = nRows;
+        _nColumns = nColumns;
+        _rowPower = static_cast<size_t>(ceilf64(log2f64(nRows)));
+    }
+
+    uint64_t inline getKey(const size_t rowIndex, const size_t columnIndex) const
+    {
+        // Maps two indices into one deterministically. We could use Cantor or Szudzik pairing functions:
+        // https://www.vertexfragment.com/ramblings/cantor-szudzik-pairing-functions/
+        // Instead we opt for the simple solution as we already know the matrix size.
+        return (rowIndex << _rowPower) + columnIndex;
+    }
+
+    void set(const size_t rowIndex, const size_t columnIndex, double value)
+    {
+        auto key = getKey(rowIndex, columnIndex);
+        data.emplace(key, value);
+    }
+
+    double at(size_t rowIndex, size_t columnIndex) const
+    {
+        auto key = getKey(rowIndex, columnIndex);
+        if (data.find(key) != data.end())
+        {
+            return data.at(key);
+        }
+
+        return 0.0;
+        
+    }
+    
+    double getValue(size_t rowIndex, size_t columnIndex)
+    {
+        auto key = getKey(rowIndex, columnIndex);
+        return data[key];
+    }
+
+    size_t rows() const { return this->_nRows; }
+    size_t columns() const { return this->_nColumns; }
+    size_t nnz() const { return this->data.size(); }
+        
+};
+
+class CooSparseMatrix : boost::noncopyable
+{
+private:
+    size_t _nRows;
+    size_t _nColumns;
+    size_t _rowPower;
+    std::vector<size_t> _rowIndices;
+    std::vector<size_t> _columnIndices;
+    std::vector<double> _values;
+
+public:
+    CooSparseMatrix() { }
+
+    void setSize(size_t nRows, size_t nColumns)
+    {
+        _nRows = nRows;
+        _nColumns = nColumns;
+    }
+
+    void add(const size_t rowIndex, const size_t columnIndex, double value)
+    {
+        _rowIndices.push_back(rowIndex);
+        _columnIndices.push_back(columnIndex);
+        _values.push_back(value);
+    }
+
+    size_t rows() const { return this->_nRows; }
+    size_t columns() const { return this->_nColumns; }
+    size_t nnz() const { return this->_values.size(); }
+    const std::vector<size_t>& rowIndices() const { return _rowIndices; }
+    const std::vector<size_t>& columnIndices() const { return _columnIndices; }
+    const std::vector<double>& values() const { return _values; }
+};
+
+
+
+class CsrMatrix : boost::noncopyable
+{
+private:
+    size_t _nRows;
+    size_t _nColumns;
+    size_t _rowPower;
+    std::vector<size_t> rowIndexPointers;
+    std::vector<size_t> columnIndices;
+    std::vector<double> values;
+
+public:
+    CsrMatrix() { }
+
+    CsrMatrix(const CooSparseMatrix &cooMatrix)
+    {
+        _nRows = cooMatrix.rows();
+        _nColumns = cooMatrix.columns();
+        const auto nnz = cooMatrix.nnz();
+        rowIndexPointers.reserve(_nRows + 1);
+        values.reserve(nnz);
+        columnIndices.reserve(nnz);
+
+        // Reset point index.
+        for (size_t i = 0; i < _nRows; i++)
+        {
+            rowIndexPointers[i] = 0;
+        }
+        
+        // Count number of non-zero column values in each row.
+        for (size_t nzi = 0; nzi < nnz; nzi++)
+        {
+            const auto cooRowIndex = cooMatrix.rowIndices()[nzi];
+            rowIndexPointers[cooRowIndex]++;
+        }
+        
+        // Compute commulative sum of non-zero values.
+        size_t cumsum = 0;
+        for (size_t i = 0; i < _nRows; i++)
+        {
+            size_t nnzColumnValues = rowIndexPointers[i];
+            rowIndexPointers[i] = cumsum;
+            cumsum += nnzColumnValues;
+        }
+
+        rowIndexPointers[_nRows] = nnz;
+
+        std::cout << "Index Pointers: \n[";
+
+        for (size_t i = 0; i < _nRows + 1; i++)
+        {
+            std::cout << rowIndexPointers[i] << " | ";
+        }
+        std::cout << "]" << std::endl;
+        
+
+        for (size_t nzi = 0; nzi < nnz; nzi++)
+        {
+            const auto cooRowIndex = cooMatrix.rowIndices()[nzi];
+            const auto dest = rowIndexPointers[cooRowIndex];
+
+            std::cout <<"For " << nzi << " -> dest: " << dest<< " " << std::endl;
+
+            columnIndices[dest] = cooMatrix.columnIndices()[nzi];
+            values[dest] = cooMatrix.values()[nzi];
+
+            // Assumes that column indices are sorted.
+            rowIndexPointers[cooRowIndex]++;
+        }
+
+        // Restore index pointers.
+        for (size_t i = 0, last = 0; i <= _nRows; i++)
+        {
+            size_t temp = rowIndexPointers[i];
+            rowIndexPointers[i]  = last;
+            last   = temp;
+        }
+    }
+
+    double at(size_t rowIndex, size_t columnIndex) const
+    {
+        auto nzStart = rowIndexPointers[rowIndex];
+        auto nzEnd = rowIndexPointers[rowIndex+1];
+
+        for (size_t ni = nzStart; ni < nzEnd; ni++)
+        {
+            if (columnIndices[ni] == columnIndex)
+            {
+                return values[ni];
+            }
+        }
+
+        return 0.0;
+    }
+};
+
+
+class SparseMatrix : boost::noncopyable
+{
+public:
+
+private:
+    size_t _nRows;
+    size_t _nColumns;
+    size_t _rowPower;
+    robin_hood::unordered_node_map<uint64_t, double> data;
+    std::vector<size_t> rowIndices;
+    std::vector<size_t> columnIndices;
+    std::vector<double> values;
+
+public:
+    SparseMatrix() { }
+
+    void setSize(size_t nRows, size_t nColumns, size_t nonZeros)
+    {
+        // data = std::make_shared<ublas::compressed_matrix<double>>(nRows, nColumns, nonZeros);
+
+        _nRows = nRows;
+        _nColumns = nColumns;
+        _rowPower = static_cast<size_t>(ceilf64(log2f64(nRows)));
+
+        rowIndices.reserve(nRows + 1);
+        if (nonZeros > 0)
+        {
+            values.reserve(nonZeros);
+            columnIndices.reserve(nonZeros);
+        }
+        
+    }
+
+    uint64_t getKey(const size_t rowIndex, const size_t columnIndex) const
+    {
+        // Maps two indices into one deterministically. We could use Cantor or Szudzik pairing functions:
+        // https://www.vertexfragment.com/ramblings/cantor-szudzik-pairing-functions/
+        // Instead we opt for the simple solution as we already know the matrix size.
+        return (rowIndex << _rowPower) + columnIndex;
+    }
+
+    void set(const size_t rowIndex, const size_t columnIndex, double value)
+    {
+        auto key = getKey(rowIndex, columnIndex);
+        data.emplace(key, value);
+        //data[key] = value;
+        // (*data)(rowIndex, columnIndex) = value;
+        // if (value == 0.0)
+        // {
+        //     if (data.find(key) != data.end())
+        //     {
+        //         data.erase(key);
+        //     }
+        // }
+        // else
+        // {
+        //      data[key] = value;
+        // }
+    }
+
+    double at(size_t rowIndex, size_t columnIndex) const
+    {
+        // return (*data)(rowIndex, columnIndex);
+        auto key = getKey(rowIndex, columnIndex);
+        if (data.find(key) != data.end())
+        {
+            return data.at(key);
+        }
+
+        return 0.0;
+        
+    }
+    
+    double getValue(size_t rowIndex, size_t columnIndex)
+    {
+        auto key = getKey(rowIndex, columnIndex);
+        return data[key];
+        // return (*data)(rowIndex, columnIndex);
+    }
+
+    size_t rows() const { return this->_nRows; }
+    size_t columns() const { return this->_nColumns; }
+    // size_t nnz() const { return this->data->nnz(); }
+    size_t nnz() const { return this->data.size(); }
+        
+};
 
 class Matrix : boost::noncopyable
 {
